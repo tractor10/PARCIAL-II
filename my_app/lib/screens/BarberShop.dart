4acb242261ecb621501785b9729c6b52606f0361
Jesús Barros <104512832+tractor10@users.noreply.github.com>
2023-04-27 07:13:08 -0500

    Add::Sections Names

--- my_app/lib/screens/BarberShop.dart
+++ my_app/lib/screens/BarberShop.dart
@@ -75,53 +75,68 @@ Widget _services(context) {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             GestureDetector(
-              onTap: () {
-                _showModal(context);
-              },
-              child: Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Color(0XFFFFC38F),
-                ),
-                child: Image.asset(
-                  'assets/imgs/tijeras.png',
-                ),
-              ),
-            ),
+                onTap: () {
+                  _showModal(context);
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(bottom: 8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Color(0XFFFFC38F),
+                      ),
+                      child: Image.asset(
+                        'assets/imgs/tijeras.png',
+                      ),
+                    ),
+                    Text('Hair cut'),
+                  ],
+                )),
             GestureDetector(
-              onTap: () {
-                _showModal(context);
-              },
-              child: Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Color(0XFFFFC38F),
-                ),
-                child: Image.asset(
-                  'assets/imgs/navaja-de-afeitar.png',
-                ),
-              ),
-            ),
+                onTap: () {
+                  _showModal(context);
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(bottom: 8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Color(0XFFFFC38F),
+                      ),
+                      child: Image.asset(
+                        'assets/imgs/navaja-de-afeitar.png',
+                      ),
+                    ),
+                    Text('Shaving'),
+                  ],
+                )),
             GestureDetector(
-              onTap: () {
-                _showModal(context);
-              },
-              child: Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Color(0XFFFFC38F),
-                ),
-                child: Image.asset(
-                  'assets/imgs/peine.png',
-                ),
-              ),
-            ),
+                onTap: () {
+                  _showModal(context);
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(bottom: 8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Color(0XFFFFC38F),
+                      ),
+                      child: Image.asset(
+                        'assets/imgs/peine.png',
+                      ),
+                    ),
+                    Text('Hair style'),
+                  ],
+                )),
           ],
         ),
       ],
@@ -145,29 +160,47 @@ Widget _barbers(context) {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Color(0XFFFFC38F),
-              ),
+            Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.only(bottom: 8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Color(0XFFFFC38F),
+                  ),
+                ),
+                Text('Bad Bunny'),
+              ],
             ),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Color(0XFFFFC38F),
-              ),
+            Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.only(bottom: 8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Color(0XFFFFC38F),
+                  ),
+                ),
+                Text('Michel'),
+              ],
             ),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Color(0XFFFFC38F),
-              ),
+            Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.only(bottom: 8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Color(0XFFFFC38F),
+                  ),
+                ),
+                Text('Antonio'),
+              ],
             ),
           ],
         ),
